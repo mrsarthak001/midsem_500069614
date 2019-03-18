6492933c8b5d605e35ec486972a04fa67ecfdc0b
sarthak <mrsarthak001@gmail.com>
2019-03-18 13:06:06 +0530

    question 2.2

--- calci.cpp
+++ calci.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+include<iostream>
 using namespace std;
 	float add(float, float);
 	float subtract(float, float);
@@ -14,3 +14,38 @@ public:
 	float multiply(float, float);
 	float divide(float, float);
 };
+
+int main()
+{
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator either + or - or * or /: ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch(op)
+    {
+        case '+':
+            cout << add(num1, num2);
+            break;
+
+        case '-':
+            cout << subtract(num1, num2);
+            break;
+
+        case '*':
+            cout << multiply(num1, num2);
+            break;
+
+        case '/':
+            cout <<  divide(num1, num2);
+            break;
+
+        default:
+
+            cout << "Error! operator is not correct";
+            break;
+    }

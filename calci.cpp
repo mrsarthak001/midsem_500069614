aadf52177c87c78b712dac60d7146138ec35bef4
sarthak <mrsarthak001@gmail.com>
2019-03-18 13:04:08 +0530

    question 2.1

---
+++ calci.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+	float add(float, float);
+	float subtract(float, float);
+	float multiply(float, float);
+	float divide(float, float);
+
+class Calculator
+{
+	float a, b;
+public:
+	float add(float, float);
+	float subtract(float, float);
+	float multiply(float, float);
+	float divide(float, float);
+};

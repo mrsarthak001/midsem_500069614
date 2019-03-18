96248532aff7bdc108b0c35151a0d7eb88f28d16
sarthak <mrsarthak001@gmail.com>
2019-03-18 13:07:15 +0530

    question 2.3

--- calci.cpp
+++ calci.cpp
@@ -49,3 +49,27 @@ int main()
             cout << "Error! operator is not correct";
             break;
     }
+float add(float num1, float num2)
+{
+
+    return(num1+num2);
+
+}
+float subtract(float num1, float num2)
+{
+
+    return(num1-num2);
+
+}
+float multiply(float num1,float num2)
+{
+
+    return(num1*num2);
+
+}
+float divide(float num1,float num2)
+{
+
+    return(num1/num2);
+
+}}
